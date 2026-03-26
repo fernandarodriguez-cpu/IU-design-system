5c5f1640cb37216a1b1e29b0799ff7d2f1c263ea
Daniel Lezama <daniel.lezama@hiuman.edu.mx>
2026-03-26 17:38:31 -0600

    update to 2.7.0 label

--- src/app/components/layout/AppShell.tsx
+++ src/app/components/layout/AppShell.tsx
@@ -440,7 +440,7 @@ export function AppShell() {
               backgroundColor: 'var(--khor-success-light)',
               color: 'var(--khor-success)',
             }}>
-              v2.6.0
+              v2.7.0
             </span>
           </div>
           <div style={{ display: 'flex', alignItems: 'center', gap: 12 }}>
