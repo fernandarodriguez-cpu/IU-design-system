fa3c138a4e4fd636f3c4968b296b1801cc2c0215
Daniel Lezama <daniel.lezama@hiuman.edu.mx>
2026-04-07 15:52:20 -0600

    Late update error handler 070426

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from 'react-router';
+// Import router (now resolves to .tsx automatically)
 import { router } from './routes';
 import { KToastProvider } from './components/design-system/organisms/KToast';
 import { KModalProvider } from './components/design-system/organisms/KModal';
